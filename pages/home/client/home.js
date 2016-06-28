c7aecc0fe209f5001ee242890388c59da7825c89
mmcgregor123 <michael@Michaels-MacBook-Pro.local>
2016-06-28 09:54:03 -0400

    PA 2

---
+++ pages/home/client/home.js
@@ -0,0 +1,39 @@
+Template.home.onCreated(function() {
+  this.state = new ReactiveDict();
+  this.state.setDefault({
+    color: "bg-info",
+    counter: 0,
+  });
+
+
+console.log("creating the template");
+console.dir(this.state);
+});
+
+
+Template.home.helpers({
+	theColor: function(){
+		const instance= Template.instance();
+		const c = instance.state.get("color")
+		return c;
+	},
+
+	theCounter: function(){
+		const instance=Template.instance();
+		return instance.state.get("counter");
+	},
+});
+
+Template.home.events({
+	"change .js-color": function(event, instance){
+		const c = instance.$(".js-color").val();
+		instance.state.set("color", c);
+	},
+
+	"click .js-pusher": function(event, instance){
+		const counter= instance.state.get("counter");
+		instance.state.set("counter", 1+counter);
+
+	}
+
+});
